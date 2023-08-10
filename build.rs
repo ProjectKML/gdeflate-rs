91d8c80b77f3c4cfe63f9311d38dcfd6b31c3037
BeastLe9enD <beastle9end@outlook.de>
2023-08-10 22:42:48 +0200

    🐛 Fix linux compile

--- build.rs
+++ build.rs
@@ -1,11 +1,15 @@
 fn main() {
-    cxx_build::bridge("src/lib.rs")
-        .include("src")
-        .file("src/GDeflateCompress.cpp")
-        .file("src/GDeflateDecompress.cpp")
+    cc::Build::new()
         .include("vendor/libdeflate")
         .file("vendor/libdeflate/lib/gdeflate_compress.c")
         .file("vendor/libdeflate/lib/gdeflate_decompress.c")
         .file("vendor/libdeflate/lib/utils.c")
+        .compile("gdeflate_c");
+
+    cxx_build::bridge("src/lib.rs")
+        .include("src")
+        .include("vendor/libdeflate")
+        .file("src/GDeflateCompress.cpp")
+        .file("src/GDeflateDecompress.cpp")
         .compile("gdeflate_cxx");
 }
