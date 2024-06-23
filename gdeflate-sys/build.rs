1717d1b27cc57c2e8c1783a5d9337fe229b5c5f1
BeastLe9enD <beastle9end@outlook.de>
2024-06-23 17:37:56 +0200

    🐛 Fix compilation on linux

--- gdeflate-sys/build.rs
+++ gdeflate-sys/build.rs
@@ -23,8 +23,16 @@ fn main() {
         .include("vendor/libdeflate")
         .file("vendor/libdeflate/lib/gdeflate_compress.c")
         .file("vendor/libdeflate/lib/gdeflate_decompress.c")
-        .file("vendor/libdeflate/lib/utils.c")
-        .compile("gdeflate_sys_cc");
+        .file("vendor/libdeflate/lib/utils.c");
+
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    build.file("vendor/libdeflate/lib/x86/cpu_features.c");
+
+    #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
+    build.file("vendor/libdeflate/lib/arm/cpu_features.c");
+
+    build.compile("gdeflate_sys_cc");
 
     generate_bindings();
 }
